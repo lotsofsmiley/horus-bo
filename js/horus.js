929d91d9ad7eabc219a50cade88c246b051d8c4e
Filipe Guimarães <manolipas69@gmail.com>
2023-10-04 16:24:36 +0100

    index tweaks

--- js/horus.js
+++ js/horus.js
@@ -1,32 +1,75 @@
-//  References to buttons
-const addUsersButton = document.getElementById("addUsersButton");
-const departmentsButton = document.getElementById("departmentsButton");
-const autoTaskButton = document.getElementById("autoTaskButton");
-//  References to Sidebars
-const addUsersSidebar = document.getElementById("addUsersSidebar");
-const departmentsSidebar = document.getElementById("departmentsSidebar");
-const autoTaskSidebar = document.getElementById("autoTaskSidebar");
+// JavaScript
+var openSidebarId = null; // Variable to track the currently open sidebar
 
-// Function to open a specific sidebar
-function openSidebar(sidebar) {
-    sidebar.style.display = "block";
-}
-
-// Add click event listeners to buttons to open respective sidebars
-addUsersButton.addEventListener("click", () => openSidebar(addUsersSidebar));
-departmentsButton.addEventListener("click", () => openSidebar(departmentsSidebar));
-autoTaskButton.addEventListener("click", () => openSidebar(autoTaskSidebar));
-
-// Get references to the sidebar and buttons
-const sidebar = document.querySelector('.sidebar');
-const buttons = document.querySelectorAll('.sidebar-button');
-
-// Function to open/close the sidebar
-function toggleSidebar() {
-    sidebar.classList.toggle('sidebar-open');
-}
-
-// Add click event listeners to the buttons
-buttons.forEach((button) => {
-    button.addEventListener('click', toggleSidebar);
+document.getElementById("addUsersButton").addEventListener("click", function () {
+    toggleSidebar("addUsersSidebar", "addUsersButton");
 });
+
+document.getElementById("departmentsButton").addEventListener("click", function () {
+    toggleSidebar("departmentsSidebar", "departmentsButton");
+});
+
+document.getElementById("autoTaskButton").addEventListener("click", function () {
+    toggleSidebar("autoTaskSidebar", "autoTaskButton");
+});
+
+function toggleSidebar(sidebarId, buttonId) {
+    if (openSidebarId === sidebarId) {
+        // If the clicked sidebar is already open, close it instantly and deselect the button
+        closeSidebar(sidebarId, buttonId);
+        openSidebarId = null;
+    } else {
+        // Close the currently open sidebar (if any) instantly and deselect its corresponding button
+        if (openSidebarId) {
+            closeSidebar(openSidebarId, getButtonId(openSidebarId), true); // Pass true to close instantly
+        }
+
+        // Open the clicked sidebar with a transition and select the button (Add the 'selected' class to the clicked button)
+        openSidebar(sidebarId, buttonId);
+        openSidebarId = sidebarId;
+    }
+}
+
+function openSidebar(sidebarId, buttonId) {
+    var sidebar = document.getElementById(sidebarId);
+    sidebar.style.width = "32%";
+
+    // Select the button
+    var selectedButton = document.getElementById(buttonId);
+    selectedButton.classList.add("selected");
+}
+
+function closeSidebar(sidebarId, buttonId, instant = false) {
+    var sidebar = document.getElementById(sidebarId);
+
+    if (instant) {
+        // Close instantly without transition
+        sidebar.style.transition = "none";
+        sidebar.style.width = "0px";
+        setTimeout(function () {
+            sidebar.style.transition = ""; // Reset transition
+        }, 0);
+    } else {
+        // Close with the transition
+        sidebar.style.width = "0px";
+    }
+
+    // Unselect the button
+    var selectedButton = document.getElementById(buttonId);
+    selectedButton.classList.remove("selected");
+}
+
+function getButtonId(sidebarId) {
+    // Map sidebar IDs to corresponding button IDs
+    switch (sidebarId) {
+        case "addUsersSidebar":
+            return "addUsersButton";
+        case "departmentsSidebar":
+            return "departmentsButton";
+        case "autoTaskSidebar":
+            return "autoTaskButton";
+        default:
+            return null;
+    }
+}
+
